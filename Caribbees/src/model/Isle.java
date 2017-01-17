96375727cc661e3fa8e67730137f8040f2656e11
JorisAvezard <avezard.joris@gmail.com>
2017-01-17 18:12:34 +0100

    Complément du package model

---
+++ Caribbees/src/model/Isle.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Isle {
+	
+	private String id;
+	private String name;
+	
+	public Isle() {
+		
+	}
+	
+	public Isle(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+}
