b40f71bcb9aa0ff8c9cbb8dcf8bda934f29e05ff
JorisAvezard <avezard.joris@gmail.com>
2017-01-18 13:19:12 +0100

    Commit de classes

--- Caribbees/src/business/engine/Travel.java
+++ Caribbees/src/business/engine/Travel.java
@@ -1,5 +1,37 @@
 package business.engine;
 
-public class Travel {
+import java.util.ArrayList;
+import java.util.List;
 
+import business.model.Hotel;
+
+public class Travel {
+	
+	private int price;
+	private List<Excursion> excursions = new ArrayList<Excursion>();
+	private List<Hotel> hotels = new ArrayList<Hotel>();
+	
+	public Travel() {
+		
+	}
+	
+	public Travel(int price) {
+		this.price = price;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public List<Excursion> getExcursion() {
+		return excursions;
+	}
+	
+	public List<Hotel> getHotel() {
+		return hotels;
+	}
 }
